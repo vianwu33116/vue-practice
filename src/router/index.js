9ed8a9136c75aaceedd8051c309ef054f5ac3eb0
vianwu33116 <wserena33116@gmail.com>
2024-08-11 23:24:07 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HelloWorld.vue')
+    },
+    {
+      path: '/week1',
+      name: 'week1',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/WeekOne.vue')
+    },
+    {
+      path: '/week2',
+      name: 'week2',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/WeekTwo.vue')
+    }
+  ]
+})
+
+export default router
