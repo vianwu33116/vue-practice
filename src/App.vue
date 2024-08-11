fb42af3f5f935000608054f7684f6f4df95ae810
vianwu33116 <wserena33116@gmail.com>
2024-08-11 23:23:20 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import WeekOne from './components/WeekOne.vue'
+import { RouterLink, RouterView } from 'vue-router'
 //console.log(num);
 //在 Vue 只能用 const 來宣告變數，使用 .value 來取值和更改值
 </script>
 
 <template>
-  <div>
-    <week-one />
+  <div class="overall">
+    <div class="header mb-3">
+      <RouterLink to="/">Home</RouterLink> | <RouterLink to="/Week1">Week1</RouterLink> |
+      <RouterLink to="/Week2">Week2</RouterLink> |
+    </div>
+    <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 import { createApp } from 'vue' //Vue 方法
 import App from './App.vue' //Vue 根元件
+import router from './router'
 
-createApp(App).mount('#app') //建構 Vue 環境
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app') //建構 Vue 環境
