c13a91a91ec64479b933574f1c0454464f4ea1dd
vianwu33116 <wserena33116@gmail.com>
2024-08-19 22:43:28 +0800

    first commit

---
+++ src/components/MenuList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="list-group">
+    <a
+      href="#"
+      class="list-group-item list-group-item-action"
+      v-for="item in menuList"
+      :key="item.id"
+      @click.prevent="emitAddItem(item)"
+      ><div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">{{ item.name }}</h5>
+        <small>{{ item.price }}</small>
+      </div>
+      <p class="mb-1">{{ item.description }}</p></a
+    >
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+defineProps(['menuList'])
+const emit = defineEmits(['emitAddItem'])
+const emitAddItem = (item) => {
+  emit('emitAddItem', item)
+}
+</script>
---
+++ src/components/OrderList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="card-title">
+        <h5>訂單</h5>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">品項</th>
+              <th scope="col">數量</th>
+              <th scope="col">小計</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in orderList" :key="item.id">
+              <td>{{ item.name }}</td>
+              <td>{{ item.amount }}</td>
+              <td>{{ item.price * item.amount }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="text-end">
+          備註: <span>{{ finalRemark }}</span>
+        </div>
+        <div class="text-end">
+          <h5>
+            總計: <span>{{ orderSum }}</span>
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps(['orderList', 'finalRemark', 'orderSum'])
+</script>
